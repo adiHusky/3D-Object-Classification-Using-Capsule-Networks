1b4d6082a4fe010122a631ac6a725915d755b785
Preethamalladu <47163693+Preethamalladu@users.noreply.github.com>
2019-04-26 21:16:49 -0400

    Update Convert_images_to_blob.py

--- Maya3D-Images-Dataset/Preetham_Alladu/Convert_images_to_blob.py
+++ Maya3D-Images-Dataset/Preetham_Alladu/Convert_images_to_blob.py
@@ -7,18 +7,18 @@ def convertToBinaryData(filename):
 
 direcotry_name = os.path.dirname(sys.argv[0])
 
-l = []                        ## initialize few lists which we will later use
-oid = 0
-img_id = []
-cat_img_name =[]
-img_type = []
-img_res = []
-img_name = []
-img_size = []
-blob_data = []                 ##
-x = []
-y = []
-z = []
+l = []                          ## initialize few lists which we will later use
+oid = 0                         #.
+img_id = []                     #.
+cat_img_name =[]                #.
+img_type = []                   #.
+img_res = []                    #.
+img_name = []                   #.
+img_size = []                   #.
+blob_data = []                  #.
+x = []                          #.
+y = []                          #.
+z = []                          ##
 
 
 img_res_val = "960x540"
@@ -26,7 +26,7 @@ img_type_val  = "png"
 for subdir, dirs, files in os.walk(direcotry_name):
     for file in files:
         filepath = subdir + os.sep + file
-        cat_img = filepath.replace("C:\\Users\\Preetham Reddy\\Desktop\\meh\\", "")
+        cat_img = filepath.replace(subdir, "")
 
         cat_img_name.append(cat_img)
 
@@ -40,15 +40,15 @@ for subdir, dirs, files in os.walk(direcotry_name):
 
         blob_data.append(convertToBinaryData(filepath))
 
-        e = re.findall("X\d+.", filepath)
+        e = re.findall("X\d+.", filepath)                       # find the x degree using regex
         qewg=e[0].replace("X","").replace("_", "")
         x.append(qewg)
 
-        e = re.findall("Y\d+.", filepath)
+        e = re.findall("Y\d+.", filepath)                       # find the y degree using regex
         qewg=e[0].replace("Y","").replace("_", "")
         y.append(qewg)
 
-        e = re.findall("Z\d+.", filepath)
+        e = re.findall("Z\d+.", filepath)                       # find the z degree using regex
         qewg=e[0].replace("Z","").replace("_", "")
         z.append(qewg)
 
@@ -62,7 +62,7 @@ data = {
     "image_id"  : img_id,
     "image_name": img_name,
     "image_size": img_size,
-    "image_reso": img_res,
+    "image_reso": img_res,                              # create a data dictonary with all the lists
     "image_type": img_type,
     "image_BLOB": blob_data,
     "X"         : x,
@@ -72,6 +72,6 @@ data = {
 }
 
 import pandas as pd
-dataFrame = pd.DataFrame(data)
+dataFrame = pd.DataFrame(data)                                  # convert data dictonary into a dataframe
 
-dataFrame.to_csv('C:\\Users\\Preetham Reddy\\Desktop\\final.csv',encoding='utf-8')
+dataFrame.to_csv('final.csv',encoding='utf-8')                  # export the dataFrame into a csv
